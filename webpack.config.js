3ba88f92347ea0535e1bf718b4e1a20592849ef5
Kia Salehi <ksalehi@wellesley.edu>
2018-04-29 12:16:57 -0700

    try using js instead of jsx entry file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/app.jsx",
+  entry: "./frontend/app.js",
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
