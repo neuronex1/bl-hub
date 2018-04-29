ce9a70b2c2312d47b1c044018a2051d68b0bfba5
Kia Salehi <ksalehi@wellesley.edu>
2018-04-29 13:20:20 -0700

    add back logic to app.jsx since it is loading

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,34 +1,34 @@
 'use strict';
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import NavBar from './components/NavBar.jsx';
-// import Homepage from './components/Homepage.jsx';
-// import AboutUs from './components/AboutUs.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavBar from './components/NavBar.jsx';
+import Homepage from './components/Homepage.jsx';
+import AboutUs from './components/AboutUs.jsx';
 console.log('but really what?');
 alert('i hate everything');
 
 document.addEventListener("DOMContentLoaded", function () {
     alert('this file gets loaded');
     console.log('this file is the worst');
-    //
-    // ReactDOM.render(
-    //     <NavBar />,
-    //     document.getElementById('nav-bar-container')
-    // );
-    //
-    // switch (window.location.pathname) {
-    //     case '/':
-    //         ReactDOM.render(
-    //             <Homepage />,
-    //             document.getElementById('home')
-    //         );
-    //         break;
-    //     case '/about-us':
-    //         ReactDOM.render(
-    //             <AboutUs />,
-    //             document.getElementById('about-us')
-    //         );
-    //         break;
-    // }
+
+    ReactDOM.render(
+        <NavBar />,
+        document.getElementById('nav-bar-container')
+    );
+
+    switch (window.location.pathname) {
+        case '/':
+            ReactDOM.render(
+                <Homepage />,
+                document.getElementById('home')
+            );
+            break;
+        case '/about-us':
+            ReactDOM.render(
+                <AboutUs />,
+                document.getElementById('about-us')
+            );
+            break;
+    }
 });
