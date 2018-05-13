1aa57d9e60931a467aa8502cf1b7eb44c7a85f28
Kia Salehi <ksalehi@wellesley.edu>
2018-05-13 12:13:09 -0700

    change footer

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Footer = (props) => {
     return (
         <div className='footer-contents'>
-            <p>© 2018 Brown University</p>
+            <p>© 2018 Sail High Productions</p>
         </div>
     );
 };
