976cf5d53a635d8311476951fb1c6d12d95d707b
Kia Salehi <ksalehi@wellesley.edu>
2018-05-27 19:32:30 -0700

    add mbl 2018 landing page with links/icons to sub pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,4 +6,8 @@ class StaticPagesController < ApplicationController
     def show_nested
         render template: "static_pages/#{params[:namespace]}/#{params[:page]}"
     end
+
+    def show_double_nested
+        render template: "static_pages/#{params[:namespace]}/#{params[:subspace]}/#{params[:page]}"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
     root 'home#index'
     get ':page' => 'static_pages#show'
     get ':namespace/:page' => 'static_pages#show_nested'
+    get ':namespace/:subspace/:page' => 'static_pages#show_double_nested'
 end
