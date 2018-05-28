f3dc4b240936651347c5254b990b00161b11b2d3
Kia Salehi <ksalehi@wellesley.edu>
2018-05-27 22:30:35 -0700

    make and style testimonials section

--- frontend/components/AboutUs.jsx
+++ frontend/components/AboutUs.jsx
@@ -78,7 +78,7 @@ const AboutUs = () => {
                     {renderAboutBoxes()}
                 </tbody>
             </table>
-            <div className='text-s'>Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a>, <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a>, and <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com.</a></div>
+            <div className='text-xs'>Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a>, <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a>, and <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com.</a></div>
         </React.Fragment>
     );
 }
