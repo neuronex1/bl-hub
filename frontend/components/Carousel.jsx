c5ceb2465b4d828ef342f3d4243b914683baa5ca
Kia Salehi <ksalehi@wellesley.edu>
2018-07-03 18:51:47 -0700

    add arrows to carousel

--- frontend/components/Carousel.jsx
+++ frontend/components/Carousel.jsx
@@ -6,6 +6,7 @@ class Carousel extends React.PureComponent {
     constructor(props) {
         super(props);
         this.changeSlide = this.changeSlide.bind(this);
+        this.changeSlideClick = this.changeSlideClick.bind(this);
         this.renderSubtitle = this.renderSubtitle.bind(this);
 
         this.slides = [
@@ -45,26 +46,40 @@ class Carousel extends React.PureComponent {
         }
     }
 
-    changeSlide() {
-        let nextSlideIdx = (this.state.currentSlideIdx + 1) % this.slides.length;
+    changeSlide(direction = 'next') {
+        let newSlideIdx = (this.state.currentSlideIdx + 1) % this.slides.length;
+
+        if (direction === 'prev') {
+            newSlideIdx = (this.state.currentSlideIdx - 1) % this.slides.length;
+            if (this.state.currentSlideIdx === 0) {
+                newSlideIdx = this.slides.length - 1;
+            }
+        }
+
         this.setState({
-            currentSlideIdx: nextSlideIdx
+            currentSlideIdx: newSlideIdx
         });
+    }
+
+    changeSlideClick(direction = 'next') {
+        window.clearInterval(this.slideInterval);
+        this.changeSlide(direction);
+        this.slideInterval = window.setInterval(this.changeSlide, 5000);
     }
 
     componentDidMount() {
         if (typeof window !== 'undefined') {
-            window.setInterval(this.changeSlide, 5000);
+            this.slideInterval = window.setInterval(this.changeSlide, 5000);
         }
     }
 
     componentWillUnmount() {
-        window.clearInterval(this.changeSlide);
+        window.clearInterval(this.slideInterval);
     }
 
     renderSubtitle(subtitle) {
         return subtitle.map(str => {
-            return <span className='subtitle'>{str}</span>;
+            return <span key={str} className='subtitle'>{str}</span>;
         });
     }
 
@@ -80,14 +95,18 @@ class Carousel extends React.PureComponent {
             };
 
             return (
-                <div className={className} key={idx} style={style}>
+                <div className={className} key={slide.src} style={style}>
+                    <img className='bl-icon bl-icon__arrow' src='https://s3.amazonaws.com/bl-hub/right-arrow.svg'
+                    onClick={this.changeSlideClick} />
+                    <img className='bl-icon bl-icon__arrow bl-icon__arrow--left' src='https://s3.amazonaws.com/bl-hub/right-arrow.svg'
+                    onClick={this.changeSlideClick.bind(this, 'prev')} />
                     <div className='text-container'>
                         <p className='slide-caption'>{slide.caption}</p>
                         <p className='slide-title'>{slide.title}</p>
                         <p className='slide-subtitle'>{this.renderSubtitle(slide.subtitle)}</p>
                         <a className='slide-button' href={slide.link}>
                             {slide.buttonLabel}
-                            <i class="fa fa-caret fa-caret-right" aria-hidden="true"></i>
+                            <i className="fa fa-caret fa-caret-right" aria-hidden="true"></i>
                         </a>
                     </div>
                 </div>
