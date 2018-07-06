4f0988dd10661b9e8502df313e0497931489bfa1
Kia Salehi <ksalehi@wellesley.edu>
2018-07-05 23:32:00 -0700

    add mbl blog post

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -45,6 +45,10 @@ class NavBar extends React.PureComponent {
                         link: '/mbl/faqs'
                     },
                     {
+                        title: 'Blog Post',
+                        link: '/mbl/blog'
+                    },
+                    {
                         title: '2018 Practicum',
                         link: '/mbl/2018-practicum'
                     },
