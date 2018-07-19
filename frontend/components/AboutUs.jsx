2fffb78663e4fa29107b1c7c86d0ed1e36d43245
Kia Salehi <ksalehi@wellesley.edu>
2018-07-18 23:01:10 -0700

    add figure to symposium blog post

--- frontend/components/AboutUs.jsx
+++ frontend/components/AboutUs.jsx
@@ -80,6 +80,7 @@ const AboutUs = () => {
             </table>
             <p className='text-m pull-right'>Our Hub is supported by National Science Foundation <a href='https://nsf.gov/awardsearch/showAward?AWD_ID=1707352' className='bl-link'>NeuroNex 1707352</a>.</p>
             <p className='text-m pull-right'>Website icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a>, <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a>, and <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com.</a></p>
+            <p className='text-m pull-right'>Additional icons: Clker-Free-Vector-Image (Pixabay).</p>
         </React.Fragment>
     );
 }
