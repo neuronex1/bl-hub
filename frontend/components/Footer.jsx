8b6d804cf3f42ac5c632d3e260224da6bb806a8f
Kia Salehi <ksalehi@wellesley.edu>
2018-08-10 09:46:53 -0700

    add logo to footer; remove from symposium post

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -6,6 +6,10 @@ const Footer = (props) => {
     return (
         <div className='footer-contents'>
             <p>© 2018 Sail High Productions</p>
+            <div className='text-center'>
+                <a href='https://twitter.com/biolumhub'><img class='bl-logo bl-logo--twitter' src='https://s3.amazonaws.com/bl-hub/Twitter_Logo_Blue.svg'/></a>
+                <a href='https://www.instagram.com/biolumhub/'><img class='bl-logo bl-logo--ig' src='https://s3.amazonaws.com/bl-hub/logos/IG_Glyph_Fill-min.png'/></a>
+            </div>
         </div>
     );
 };
