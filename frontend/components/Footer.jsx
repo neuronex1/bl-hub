94cd6202273a50d5cd18a59f5b725be3d4977a92
Kia Salehi <kiasalehi@kias-mbp.lan>
2018-11-18 22:38:18 -0800

    adjust footer display

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -4,12 +4,14 @@ import React from 'react';
 
 const Footer = (props) => {
     return (
-        <div className='footer-contents'>
-            <p className='float-left'>© 2018 Sail High Productions</p>
-            <p class='text-center footer-center'>The Bioluminescence Hub</p>
-            <div className='float-right'>
-                <a href='https://twitter.com/biolumhub'><img class='bl-logo bl-logo--twitter' src='https://s3.amazonaws.com/bl-hub/Twitter_Logo_Blue.svg'/></a>
-                <a href='https://www.instagram.com/biolumhub/'><img href='#' class='bl-logo bl-logo--ig' src='https://s3.amazonaws.com/bl-hub/logos/IG_Glyph_Fill-min.png'/></a>
+        <div className='footer-wrapper'>
+            <div className='footer-contents'>
+                <p>© 2018 Sail High Productions</p>
+                <p>The Bioluminescence Hub</p>
+                <div>
+                    <a href='https://twitter.com/biolumhub'><img class='bl-logo bl-logo--twitter' src='https://s3.amazonaws.com/bl-hub/Twitter_Logo_Blue.svg'/></a>
+                    <a href='https://www.instagram.com/biolumhub/'><img href='#' class='bl-logo bl-logo--ig' src='https://s3.amazonaws.com/bl-hub/logos/IG_Glyph_Fill-min.png'/></a>
+                </div>
             </div>
         </div>
     );
