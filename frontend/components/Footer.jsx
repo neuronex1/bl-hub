ce8254d9ed16af47b4d64892740d4214719b04bc
Kia Salehi <kiasalehi@kias-mbp.lan>
2018-11-18 22:15:28 -0800

    update footer

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -5,7 +5,12 @@ import React from 'react';
 const Footer = (props) => {
     return (
         <div className='footer-contents'>
-            <p>© 2018 Sail High Productions</p>
+            <p className='float-left'>© 2018 Sail High Productions</p>
+            <p class='text-center footer-center'>The Bioluminescence Hub</p>
+            <div className='float-right'>
+                <a href='https://twitter.com/biolumhub'><img class='bl-logo bl-logo--twitter' src='https://s3.amazonaws.com/bl-hub/Twitter_Logo_Blue.svg'/></a>
+                <a href='https://www.instagram.com/biolumhub/'><img href='#' class='bl-logo bl-logo--ig' src='https://s3.amazonaws.com/bl-hub/logos/IG_Glyph_Fill-min.png'/></a>
+            </div>
         </div>
     );
 };
