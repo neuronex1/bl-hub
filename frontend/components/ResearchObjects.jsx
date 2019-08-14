078a2be9ee80fae4f10aa39c76c37bad85d4d3a1
Justine <neuronex1@gmail.com>
2019-08-14 16:07:03 -0400

    added links to dissemination page and tried to adjust width of research-objects container

--- frontend/components/ResearchObjects.jsx
+++ frontend/components/ResearchObjects.jsx
@@ -15,14 +15,16 @@ const ResearchObjects = () => {
 
     return (
         <React.Fragment>
+        <div class='container-xtrawide'>
             <img class='img__fixed-width' src='https://bl-hub.s3.amazonaws.com/research-objects/Brain-key-terms-fig-min.jpg' />
             <h2 className='text-center'>Our Research</h2>
             <h3></h3>
-            <table className='research-objects__table'>
+            <table className='research-objects__table '>
                 <tbody>
                     {renderResearchObjectsBoxes(RESEARCH_OBJECTS)}
                 </tbody>
         </table>
+        </div>
         </React.Fragment>
     );
 }
