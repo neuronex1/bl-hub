2f85eb0993357af16b0006efe146b1b828d51213
Justine <neuronex1@gmail.com>
2020-04-09 16:02:16 -0400

    added theses area on research-objects and data for Rachel, Griffin, and Eric P.

--- frontend/components/ResearchObjects.jsx
+++ frontend/components/ResearchObjects.jsx
@@ -5,6 +5,7 @@ import ResearchObjectsBox from './ResearchObjectsTextBox';
 import {
     RESEARCH_OBJECTS_PAPERS,
     RESEARCH_OBJECTS_POSTERS,
+    RESEARCH_OBJECTS_THESES,
 } from './ResearchObjectsConfig.js';
 
 const ResearchObjects = () => {
@@ -19,14 +20,26 @@ const ResearchObjects = () => {
         <div class='container-xtrawide'>
             <img class='img__fixed-width' src='https://bl-hub.s3.amazonaws.com/research-objects/Brain-key-terms-fig-min.jpg' />
             <h2 className='text-center'>Our Research</h2>
+
+<h3 className='text-center'><a class='bl-link' href="#Papers">Papers and Manuscripts</a></h3>
+<h3 className='text-center'><a class='bl-link' href="#Theses">Theses</a></h3>
+<h3 className='text-center'><a class='bl-link' href="#Posters">Posters</a></h3>
+
+<div className="horizontal-divider mtxxl mbxxl" id="Papers"></div>
                 <h3 className='text-center'>Papers and Manuscripts</h3>
             <table className='research-objects__table '>
                 <tbody>
                     {renderResearchObjectsBoxes(RESEARCH_OBJECTS_PAPERS)}
                 </tbody>
             </table>
-
-                <div className="horizontal-divider mtxxl mbxxl"></div>
+<div className="horizontal-divider mtxxl mbxxl" id="Theses"></div>
+              <h3 className='text-center'>Theses</h3>
+        <table className='research-objects__table '>
+              <tbody>
+                    {renderResearchObjectsBoxes(RESEARCH_OBJECTS_THESES)}
+              </tbody>
+        </table>
+<div className="horizontal-divider mtxxl mbxxl" id="Posters"></div>
 
                 <h3 className='text-center'>Posters</h3>
                 <table className='research-objects__table'>
